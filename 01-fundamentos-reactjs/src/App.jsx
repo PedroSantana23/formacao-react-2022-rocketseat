33d1c0d6c1a563c7db3e693105cd72f1f4f6f50a
PedroSantana23 <ph.silva.santana@gmail.com>
2025-09-11 19:58:51 -0300

    Fundamentos do Reactjs -> 1) Iniciando com React

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Pedro Henrique" 
+        content="Sla" 
+      />
+      <Post 
+        author="Luísa" 
+        content="Sla2" 
+      />
+    </div>
+  )
+}
